62347fd415ebd142cd1063796ab351043fe74d05
MyNameIsDmitriy <gopanchuk.dm@mail.ru>
2023-01-18 13:50:42 +0300

    feat: calculation-history component

---
+++ src/components/calc-hystory.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="calculation-history">
+    <p>{{ calculationHistory[2] || "" }}</p>
+    <p>{{ calculationHistory[1] || "" }}</p>
+    <p>{{ calculationHistory[0] || "calculate smth to get history" }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalcHistory",
+  props: ["calculationHistory"],
+  emits: [""],
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/_variables.scss";
+
+.calculation-history {
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 5px;
+  p {
+    display: flex;
+    justify-content: flex-end;
+
+    width: 97%;
+
+    margin-bottom: 3px;
+
+    color: $color-black;
+    font-family: $font-2;
+  }
+}
+</style>
