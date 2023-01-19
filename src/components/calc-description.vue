406f9da45a9276bacb12f5b13aa0e73bfc2029b7
MyNameIsDmitriy <gopanchuk.dm@mail.ru>
2023-01-19 12:22:52 +0300

    feat: calculation-description component

---
+++ src/components/calc-description.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="keyboard-description">
+    <p>
+      {{ actualDescription || "hover your mouse over any of the buttons" }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalcDescription",
+  props: ["actualDescription"],
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/_variables.scss";
+
+.keyboard-description p {
+  color: $color-black;
+  font-size: 1.6rem;
+  font-family: $font-2;
+}
+</style>
