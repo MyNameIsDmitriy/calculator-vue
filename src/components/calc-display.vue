0ea51de55954826e86b963f780666413d1ed6a64
MyNameIsDmitriy <gopanchuk.dm@mail.ru>
2023-01-26 12:48:39 +0300

    feat: calculation-display component

---
+++ src/components/calc-display.vue
@@ -0,0 +1,65 @@
+<template>
+  <form class="calculation" @submit.prevent>
+    <input
+      class="calculation-field input"
+      type="text"
+      autofocus
+      ref="inputRef"
+      @keyup="calculateInput($event)"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <p class="calculation-result">
+      {{ calculationResult || "result is..." }}
+    </p>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "CalcDisplay",
+  props: ["calculationResult", "modelValue"],
+  emits: ["calculateInput", ""],
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    calculateInput(event) {
+      this.$emit("calculateInput", event);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/_variables.scss";
+.calculation {
+  margin: 5px 0 7px 0;
+
+  .calculation-field {
+    width: 100%;
+
+    box-sizing: border-box;
+    border: none;
+    outline: none;
+
+    background: transparent;
+    font-size: 1.8rem;
+    color: $color-white;
+    font-family: $font-1;
+
+    text-align: center;
+  }
+
+  .calculation-result {
+    display: flex;
+    justify-content: center;
+
+    color: $color-purple;
+    font-size: 1.7rem;
+    font-family: $font-2;
+  }
+}
+</style>
